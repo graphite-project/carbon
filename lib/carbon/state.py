5505662d7903dba81aca41c752ec5b7f9ee3149e
Michael Leinartas <mleinartas@gmail.com>
2012-06-04 01:49:12 -0500

    Add missing state module items for completeness

--- lib/carbon/state.py
+++ lib/carbon/state.py
@@ -3,7 +3,12 @@ This module exists for the purpose of tracking global state used across
 several modules.
 """
 
-metricReceiversPaused = False
 cacheTooFull = False
+client_manager = None
 connectedMetricReceiverProtocols = set()
 database = None
+events = None
+instrumentation = None
+metricReceiversPaused = False
+pipeline_processors = []
+settings = None

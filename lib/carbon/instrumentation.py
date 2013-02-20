9bbd35d45f220ac7ebc2fd4392ee3921f66f6d96
Jeff Forcier <jeff@bitprophet.org>
2013-02-19 21:31:33 -0800

    Fix misreporting cache size/queues metrics

--- lib/carbon/instrumentation.py
+++ lib/carbon/instrumentation.py
@@ -78,6 +78,13 @@ def recordMetrics():
     cacheQueries = myStats.get('cacheQueries', 0)
     cacheOverflow = myStats.get('cache.overflow', 0)
 
+    # Calculate cache-data-structure-derived metrics prior to storing anything
+    # in the cache itself -- which would otherwise affect said metrics.
+    cache_size = cache.MetricCache.size
+    cache_queues = len(cache.MetricCache)
+    record('cache.size', cache_size)
+    record('cache.queues', cache_queues)
+
     if updateTimes:
       avgUpdateTime = sum(updateTimes) / len(updateTimes)
       record('avgUpdateTime', avgUpdateTime)
@@ -91,8 +98,6 @@ def recordMetrics():
     record('creates', creates)
     record('errors', errors)
     record('cache.queries', cacheQueries)
-    record('cache.queues', len(cache.MetricCache))
-    record('cache.size', cache.MetricCache.size)
     record('cache.overflow', cacheOverflow)
 
   # aggregator metrics

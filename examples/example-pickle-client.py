76d96eb4b7fd83177b3e8d10193b4462451d7473
linkslice <sparctacus@gmail.com>
2013-06-27 22:02:54 -0700

    updaated copyright per sejeff

--- examples/example-pickle-client.py
+++ examples/example-pickle-client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-"""Copyright 2008 Orbitz WorldWide
+"""Copyright 2013 Bryan Irvine
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.

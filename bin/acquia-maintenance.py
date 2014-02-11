cbe9420ebcb991a5b8de07447971c874fb36f37f
Aaron Morton <aaron@thelastpickle.com>
2014-02-11 21:16:26 +1300

    acquia maintenance script to perform rollups
    see comments at the top of the files, note that this is still WIP

---
+++ bin/acquia-maintenance.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python
+"""
+Runs maintenance tasks using the carbon_cassandra_plugin.
+
+This file is a fork of `ceres-maintenace` which uses a combination of reading 
+from disk, using a CeresTree and creating CeresNodes directly. It follows the 
+same principles as `ceres-maintenace`.
+
+ 
+"""
+
+import os
+from optparse import OptionParser
+import sys
+import time
+import traceback
+
+from carbon_cassandra_plugin import carbon_cassandra_db
+
+# Make carbon imports available for some functionality
+root_dir = os.environ['GRAPHITE_ROOT'] = os.environ.get('GRAPHITE_ROOT', '/opt/graphite/')
+lib_dir = os.path.join(root_dir, 'lib')
+sys.path.append(lib_dir)
+
+try:
+  import carbon
+except ImportError:
+  print ("Failed to import carbon, specify your installation location "
+         "with the GRAPHITE_ROOT environment variable.")
+  sys.exit(1)
+
+
+EVENTS = (
+  'maintenance_start',
+  'maintenance_complete',
+  'node_found',
+  'directory_found',
+  'directory_empty',
+)
+
+
+class Plugin:
+  context = {
+    'params' : {}
+  }
+
+  def __init__(self, path):
+    self.path = path
+    self.name = os.path.basename( os.path.splitext(path)[0] )
+    self.namespace = {}
+    self.namespace.update(Plugin.context)
+    self.event_handlers = {}
+
+  def load(self):
+    execfile(self.path, self.namespace)
+    for event in EVENTS:
+      if event in self.namespace:
+        self.event_handlers[event] = self.namespace[event]
+
+  def handle_event(self, event, *args, **kwargs):
+    handler = self.event_handlers.get(event)
+    if handler:
+      handler(*args, **kwargs)
+
+
+class PluginFinder:
+  def __init__(self, plugin_dir):
+    self.plugin_dir = os.path.abspath( os.path.expanduser(plugin_dir) )
+
+  def find_plugins(self, plugin_refs):
+    for ref in plugin_refs:
+      if ref.startswith('~'):
+        ref = os.path.expanduser(ref)
+
+      if os.path.isfile(ref):
+        yield Plugin(ref)
+
+      else:
+        filename = "%s.py" % ref
+        plugin_path = os.path.join(self.plugin_dir, filename)
+
+        if os.path.isfile(plugin_path):
+          yield Plugin(plugin_path)
+        else:
+          raise PluginNotFound("The plugin '%s' could not be found in %s" % (ref, self.plugin_dir))
+
+
+class PluginNotFound(Exception):
+  pass
+
+
+class EventDispatcher:
+  def __init__(self):
+    self.handlers = {}
+
+  def add_handler(self, event, handler):
+    if event not in self.handlers:
+      self.handlers[event] = []
+    self.handlers[event].append(handler)
+
+  def dispatch(self, event, *args, **kwargs):
+    for handler in self.handlers.get(event, []):
+      try:
+        handler(*args, **kwargs)
+      except:
+        log("--- Error in %s event-handler ---" % event)
+        log( traceback.format_exc() )
+        log('-' * 80)
+
+  __call__ = dispatch
+
+
+def daemonize():
+  if os.fork() > 0: sys.exit(0)
+  os.setsid()
+  if os.fork() > 0: sys.exit(0)
+  si = open('/dev/null', 'r')
+  so = open('/dev/null', 'a+')
+  se = open('/dev/null', 'a+', 0)
+  os.dup2(si.fileno(), sys.stdin.fileno())
+  os.dup2(so.fileno(), sys.stdout.fileno())
+  os.dup2(se.fileno(), sys.stderr.fileno())
+
+
+# Utility functions (exist in the plugin namespace)
+logfile = open('/dev/null', 'w')
+
+def log(message):
+  logfile.write("[%s]  %s\n" % (time.ctime(), message.strip()))
+  logfile.flush()
+
+
+schemas = None
+def get_storage_config(path):
+  global schemas
+  if schemas is None:
+    schemas = loadStorageSchemas()
+
+  for schema in schemas:
+    if schema.matches(path):
+      return schema
+
+  raise Exception("No storage schema matched the metric '%s', check your storage-schemas.conf file." % path)
+
+
+class MissingRequiredParam(Exception):
+  def __init__(self, param):
+    Exception.__init__(self)
+    self.param = param
+
+
+class PluginFail(Exception):
+  pass
+
+def _walk(tree, dispatch, nodePath):
+  """Recursively walk the self and childs nodes in `tree` below `nodePath` 
+  calling the `dispatch` function for each visit.
+  """
+  
+  childs = tree.selfAndChildPaths(nodePath)
+  if not childs:
+    dispatch('directory_empty', nodePath)
+    return
+  
+  for child, isMetric in childs:
+    if isMetric:
+      if child != nodePath:
+        dispatch('node_found', tree.getNode(child))
+      else:
+        _walk(tree, dispatch, child)
+    else:
+      if (child != nodePath) and (nodePath != "*"):
+        dispatch('directory_found', child)
+      _walk(tree, dispatch, child)
+  return
+  
+if __name__ == '__main__':
+  default_plugindir = os.path.join(root_dir, 'plugins', 'maintenance')
+  parser = OptionParser(usage='''%prog [options] plugin [plugin2 ...] [key=val ...]''')
+  parser.add_option('--daemon', action='store_true')
+  parser.add_option('--verbose', action='store_true', help="Increase truthiness")
+  parser.add_option('--log', help="Write to the given log file instead of stdout")
+  parser.add_option('--root', default='/opt/graphite/storage/ceres/',
+                    help="Specify were to perform maintenance (default: /opt/graphite/storage/ceres/)")
+  parser.add_option('--plugindir', default=default_plugindir,
+                    help="Specify path to the plugin directory (default: %s)" % default_plugindir)
+  parser.add_option('--configdir', default=None, help="Path to the carbon-daemon instance configuration directory")
+
+  options, args = parser.parse_args()
+
+  if not options.configdir:
+    sys.exit("You must specify a --configdir for the carbon-daemon instance config directory "
+             "that has the storage-rules.conf you want ceres-maintenance to use.")
+
+  # Magic plugin vars
+  Plugin.context['log'] = log
+  Plugin.context['MissingRequiredParam'] = MissingRequiredParam
+  Plugin.context['PluginFail'] = PluginFail
+
+  from carbon.conf import settings, load_storage_rules
+  settings.use_config_directory(options.configdir)
+  Plugin.context['storage_rules'] = settings['STORAGE_RULES'] = load_storage_rules(settings)
+
+  # User-defined plugin vars
+  plugin_args = []
+  for arg in args:
+    if '=' in arg:
+      key, value = arg.split('=')
+      Plugin.context['params'][key] = value
+    else:
+      plugin_args.append(arg)
+
+  if len(plugin_args) < 1:
+    print "At least one plugin is required."
+    parser.print_usage()
+    sys.exit(1)
+
+  # Load the plugins and setup event handlers
+  finder = PluginFinder(options.plugindir)
+  try:
+    plugins = finder.find_plugins(plugin_args)
+  except PluginNotFound, e:
+    print e.message, ' searched in %s' % options.plugindir
+    sys.exit(1)
+  dispatcher = EventDispatcher()
+
+  def dispatch(event, *args):
+    if options.verbose:
+      log("%s :: %s" % (event, args))
+    dispatcher(event, *args)
+
+  for plugin in plugins:
+    try:
+      plugin.load()
+    except MissingRequiredParam, e:
+      print "Failed to load %s plugin: required param '%s' must be specified" % (plugin.name, e.param)
+      sys.exit(1)
+    except PluginFail, e:
+      print "Failed to load %s plugin: %s" % (plugin.name, e.message)
+      sys.exit(1)
+    for event, handler in plugin.event_handlers.items():
+      dispatcher.add_handler(event, handler)
+
+  # Daemonize & logify
+  if options.daemon:
+    daemonize()
+
+  if options.log:
+    logfile = open(options.log, 'a')
+
+  if not (options.daemon or options.log):
+    logfile = sys.stdout
+  
+  # TODO: add the server names and keyspace as command line args. 
+  tree = carbon_cassandra_db.DataTree("/", "graphite", ["172.16.1.18"])
+
+    # Begin walking the tree
+  dispatch('maintenance_start', tree)
+  _walk(tree, dispatch, "*")
+  dispatch('maintenance_complete', tree)
+
+
+
---
+++ plugins/maintenance/acquia_rollup.py
@@ -0,0 +1,139 @@
+"""
+A maintenance plugin designed to work with the `acquia-maintenance` script. 
+
+This script is a fork of `rollup.py` designed to work with the 
+`carbon_cassandra_plugin`.
+"""
+import time
+
+from carbon_cassandra_plugin import carbon_cassandra_db
+
+#######################################################
+# Put your custom aggregation logic in this function! #
+#######################################################
+def aggregate(node, datapoints):
+  "Put your custom aggregation logic here."
+  values = [value for (timestamp,value) in datapoints if value is not None]
+  metadata = node.readMetadata()
+  method = metadata.get('aggregationMethod', 'avg')
+
+  if method in ('avg', 'average'):
+    return float(sum(values)) / len(values) # values is guaranteed to be nonempty
+
+  elif method == 'sum':
+    return sum(values)
+
+  elif method == 'min':
+    return min(values)
+
+  elif method == 'max':
+    return max(values)
+
+  elif method == 'median':
+    values.sort()
+    return values[ len(values) / 2 ]
+
+
+def node_found(node):
+  archives = []
+  t = int( time.time() )
+  metadata = node.readMetadata()
+
+  for (precision, retention) in metadata['retentions']:
+    archiveEnd =  t - (t % precision)
+    archiveStart = archiveEnd - (precision * retention)
+    t = archiveStart
+    archives.append({
+      'precision' : precision,
+      'retention' : retention,
+      'startTime' : archiveStart,
+      'endTime' : archiveEnd,
+      'slices' : [s for s in node.slices if s.timeStep == precision]
+    })
+
+  for i, archive in enumerate(archives):
+    if i == len(archives) - 1:
+      do_rollup(node, archive, None)
+    else:
+      do_rollup(node, archive, archives[i+1])
+
+
+
+def do_rollup(node, fineArchive, coarseArchive):
+  
+  # TODO: Remove debugging code.
+  print "Rollup called on node %s from %s to %s" % (node.nodePath, 
+    fineArchive, coarseArchive)
+  overflowSlices = [s for s in fineArchive['slices'] if s.startTime < fineArchive['startTime']]
+  if not overflowSlices:
+    return
+
+  if coarseArchive is None: # delete the old datapoints
+    # TODO: Ensure old data points are deleted using TTL or modern dance
+    # we use TTL to delete data
+    #   for slice in overflowSlices:
+    #     try:
+    #       slice.deleteBefore(fineArchive['startTime'])
+    #     except SliceDeleted:
+    #       pass
+    pass
+  else:
+    overflowDatapoints = []
+    for slice in overflowSlices:
+      datapoints = slice.read(slice.startTime, fineArchive['startTime'])
+      overflowDatapoints.extend( list(datapoints) )  
+    overflowDatapoints.sort()
+    
+    fineStep = fineArchive['precision']
+    coarseStep = coarseArchive['precision']
+    deletePriorTo = coarseArchive['startTime'] + (coarseStep * coarseArchive['retention'])
+    
+    metadata = node.readMetadata()
+    xff = metadata.get('xFilesFactor')
+
+    # We define a window corresponding to exactly one coarse datapoint
+    # Then we use it to select datapoints for aggregation
+    for i in range(coarseArchive['retention']):
+      windowStart = coarseArchive['startTime'] + (i * coarseStep)
+      windowEnd = windowStart + coarseStep
+
+      fineDatapoints = [d for d in overflowDatapoints if d[0] >= windowStart and d[0] < windowEnd]
+      
+      if fineDatapoints:
+        knownValues = [value for (timestamp,value) in fineDatapoints if value is not None]
+        if not knownValues:
+          continue
+        knownPercent = float(len(knownValues)) / len(fineDatapoints)
+        if knownPercent < xff:  # we don't have enough data to aggregate!
+          continue
+        
+        coarseValue = aggregate(node, fineDatapoints)
+        coarseDatapoint = (windowStart, coarseValue)
+        fineValues = [d[1] for d in fineDatapoints]
+
+        written = False
+        for slice in coarseArchive['slices']:
+          if slice.startTime <= windowStart and slice.endTime >= windowStart:
+            slice.write([coarseDatapoint])
+            written = True
+            break
+
+          # We could pre-pend to an adjacent slice starting after windowStart
+          # but that would be much more expensive in terms of I/O operations.
+          # In the common case, append-only is best.
+
+        if not written:
+          newSlice = carbon_cassandra_db.DataSlice.create(node, windowStart, 
+            coarseStep)
+          newSlice.write([coarseDatapoint])
+          coarseArchive['slices'].append(newSlice)
+          deletePriorTo = min(deletePriorTo, windowStart)
+
+    # TODO: ensure overflow data is deleted via TTL or classic tap.
+    
+    # Delete the overflow from the fine archive
+    # for slice in overflowSlices:
+    #   try:
+    #     slice.deleteBefore( deletePriorTo ) # start of most recent coarse datapoint
+    #   except SliceDeleted:
+    #     pass

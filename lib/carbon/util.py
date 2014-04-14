d8dd79cd697e5f97b7e61cb44a984ccd28dc5bd5
johnseekins <john.seekins@gmail.com>
2014-04-14 09:43:32 -0600

    Apply files from https://github.com/posix4e/graphite-data

--- lib/carbon/util.py
+++ lib/carbon/util.py
@@ -155,10 +155,37 @@ else:
     @classmethod
     def loads(cls, pickle_string):
       return cls(StringIO(pickle_string)).load()
- 
 
 def get_unpickler(insecure=False):
   if insecure:
     return pickle
-  else:
+  else:def aggregate(aggregationMethod, knownValues):
+    if aggregationMethod == 'average':
+        return float(sum(knownValues)) / float(len(knownValues))
+    elif aggregationMethod == 'sum':
+        return float(sum(knownValues))
+    elif aggregationMethod == 'last':
+        return knownValues[len(knownValues) - 1]
+    elif aggregationMethod == 'max':
+        return max(knownValues)
+    elif aggregationMethod == 'min':
+        return min(knownValues)
+    else:
+        raise Exception("Unrecognized aggregation method %s" %
+                        aggregationMethod)
     return SafeUnpickler
+
+def aggregate(aggregationMethod, knownValues):
+    if aggregationMethod == 'average':
+        return float(sum(knownValues)) / float(len(knownValues))
+    elif aggregationMethod == 'sum':
+        return float(sum(knownValues))
+    elif aggregationMethod == 'last':
+        return knownValues[len(knownValues) - 1]
+    elif aggregationMethod == 'max':
+        return max(knownValues)
+    elif aggregationMethod == 'min':
+        return min(knownValues)
+    else:
+        raise Exception("Unrecognized aggregation method %s" %
+                        aggregationMethod)

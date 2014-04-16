b77da9a2f2346a7243cf3207f42a0239c4d7e530
johnseekins <john.seekins@oracle.com>
2014-04-16 15:14:31 -0600

    Fixed a *very* strange bug where code was duplicated, fixed bugs in init scripts

--- lib/carbon/util.py
+++ lib/carbon/util.py
@@ -165,20 +165,7 @@ else:
 def get_unpickler(insecure=False):
   if insecure:
     return pickle
-  else:def aggregate(aggregationMethod, knownValues):
-    if aggregationMethod == 'average':
-        return float(sum(knownValues)) / float(len(knownValues))
-    elif aggregationMethod == 'sum':
-        return float(sum(knownValues))
-    elif aggregationMethod == 'last':
-        return knownValues[len(knownValues) - 1]
-    elif aggregationMethod == 'max':
-        return max(knownValues)
-    elif aggregationMethod == 'min':
-        return min(knownValues)
-    else:
-        raise Exception("Unrecognized aggregation method %s" %
-                        aggregationMethod)
+  else:
     return SafeUnpickler
 
 def aggregate(aggregationMethod, knownValues):

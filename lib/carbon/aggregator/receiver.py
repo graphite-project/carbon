graphite-project/carbon

commit 4424e467d04065061057754a460e0a964c4dd2e7

added warning if the aggregator is misconfigured

lib/carbon/aggregator/receiver.py
@@ -2,7 +2,7 @@ from carbon.instrumentation import increment
 from carbon.aggregator.rules import RuleManager
 from carbon.aggregator.buffers import BufferManager
 from carbon.rewrite import RewriteRuleManager
-from carbon import events
+from carbon import events, log
 
 
 def process(metric, datapoint):
@@ -32,4 +32,5 @@ def process(metric, datapoint):
     metric = rule.apply(metric)
 
   if metric not in aggregate_metrics:
+    log.msg("Couldn't match metric %s with any aggregation rule. Passing on un-aggregated." % metric)
     events.metricGenerated(metric, datapoint)

commit aec532a94f6aa88bef70e2445b0ab5dfbcb88a8f

aggregator.receiver: correct test for unaggregated diagnostic
The test added in commit 4d87d706a4d3cf9dad33761cb1bcece5644d95a6 was to
avoid clobbering an incoming metric that was changed by aggregation
(viz. an output aggregation matches the input metric).  The diagnostic
added in 4424e467d04065061057754a460e0a964c4dd2e7 mistakenly suggests
that the metric was not aggregated in this case.  In fact, the original
situation is highly unlikely to occur so in most cases the original
metric will be forwarded as expected, but the diagnostic should be
emitted only (a) when such diagnostics are requested and (b) when the
metric did not contribute to an aggregation.

Signed-off-by: Peter A. Bigot <pab@pabigot.com>

lib/carbon/aggregator/receiver.py
@@ -13,14 +13,15 @@ def process(metric, datapoint):
     metric = rule.apply(metric)
 
   aggregate_metrics = []
+  aggregated = False
 
   for rule in RuleManager.rules:
     aggregate_metric = rule.get_aggregate_metric(metric)
 
     if aggregate_metric is None:
       continue
-    else:
-      aggregate_metrics.append(aggregate_metric)
+    aggregate_metrics.append(aggregate_metric)
+    aggregated = True
 
     buffer = BufferManager.get_buffer(aggregate_metric)
 
@@ -33,6 +34,6 @@ def process(metric, datapoint):
     metric = rule.apply(metric)
 
   if metric not in aggregate_metrics:
-    if settings.LOG_AGGREGATOR_MISSES:
+    if settings.LOG_AGGREGATOR_MISSES and not aggregated:
       log.msg("Couldn't match metric %s with any aggregation rule. Passing on un-aggregated." % metric)
     events.metricGenerated(metric, datapoint)
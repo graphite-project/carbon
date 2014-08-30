db0669b33ff13618ba9af590a288effbfa3817bf
Denis Zhdanov <denis.zhdanov@gmail.com>
2014-08-30 21:16:21 +0200

    log.err is more suitable here

--- lib/carbon/writer.py
+++ lib/carbon/writer.py
@@ -122,7 +122,7 @@ def writeCachedDataPoints():
                 settings.WHISPER_FALLOCATE_CREATE)
             instrumentation.increment('creates')
         except:
-            log.msg("Error creating %s" % (dbFilePath))
+            log.err("Error creating %s" % (dbFilePath))
             continue
       # If we've got a rate limit configured lets makes sure we enforce it
       if UPDATE_BUCKET:

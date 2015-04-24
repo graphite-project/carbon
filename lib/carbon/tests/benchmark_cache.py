6d3893439e13c15b29d6388b39a1981602b4a726
Scott Sanders <jssjr@github.com>
2015-04-24 19:36:32 -0400

    Benchmark inserting datapoints, and inserting new metrics

--- lib/carbon/tests/benchmark_cache.py
+++ lib/carbon/tests/benchmark_cache.py
@@ -8,18 +8,33 @@ settings = {
         'CACHE_WRITE_STRATEGY': 'naive'
         }
 metric_cache = _MetricCache(method="naive")
-i=0
+i = 0
 
-def store_command():
+def reset(method):
+    i = 0
+    metric_cache = _MetricCache(method=method)
+
+def command_store_foo():
     global i
     i=i+1
     return metric_cache.store('foo', (i, 1.0))
 
+def command_store_foo_n():
+    global i
+    i=i+1
+    return metric_cache.store("foo.%d" % i, (i, 1.0))
+
 if __name__ == '__main__':
     import timeit
 
-    print("Benchmarking MetricCache.store()...")
+    print("Benchmarking single metric MetricCache.store()...")
     print("datapoints time")
     for n in [1000, 10000, 100000, 1000000]:
-        metric_cache = _MetricCache(method="naive")
-        print("%d\t%f" % (n, timeit.timeit(store_command, number=n)))
+        reset("naive")
+        print("%d\t%f" % (n, timeit.timeit(command_store_foo, number=n)))
+
+    print("Benchmarking unique metric MetricCache.store()...")
+    print("datapoints time")
+    for n in [1000, 10000, 100000, 1000000]:
+        reset("naive")
+        print("%d\t%f" % (n, timeit.timeit(command_store_foo_n, number=n)))

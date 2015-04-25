e85648a31a5b23cdad71398e1020364975ed0ad2
Scott Sanders <jssjr@github.com>
2015-04-24 21:00:16 -0400

    More benchmarks

--- lib/carbon/tests/benchmark_cache.py
+++ lib/carbon/tests/benchmark_cache.py
@@ -12,7 +12,6 @@ i = 0
 
 def reset(method):
     i = 0
-    metric_cache = _MetricCache(method=method)
 
 def command_store_foo():
     global i
@@ -24,6 +23,14 @@ def command_store_foo_n():
     i=i+1
     return metric_cache.store("foo.%d" % i, (i, 1.0))
 
+def command_isFull():
+    return metric_cache.isFull()
+
+def command_drain():
+    while metric_cache:
+        metric_cache.pop()
+    return metric_cache.size
+
 if __name__ == '__main__':
     import timeit
 
@@ -31,10 +38,41 @@ if __name__ == '__main__':
     print("datapoints time")
     for n in [1000, 10000, 100000, 1000000]:
         reset("naive")
+        metric_cache = _MetricCache(method="naive")
         print("%d\t%f" % (n, timeit.timeit(command_store_foo, number=n)))
 
     print("Benchmarking unique metric MetricCache.store()...")
     print("datapoints time")
     for n in [1000, 10000, 100000, 1000000]:
         reset("naive")
+        metric_cache = _MetricCache(method="naive")
         print("%d\t%f" % (n, timeit.timeit(command_store_foo_n, number=n)))
+
+    print("Benchmarking MetricCache.isFull()...")
+    print("cache_size time")
+    for n in [1000, 10000, 100000, 1000000]:
+        reset("naive")
+        metric_cache = _MetricCache(method="naive")
+        timeit.timeit(command_store_foo, number=n)
+        print("%d\t%f" % (n, timeit.timeit(command_isFull)))
+
+    print("Benchmarking single metric MetricCache drain...")
+    print("strategy cache_size time end_size")
+    for strategy in ["sorted", "max", "naive"]:
+        for n in [1000, 10000, 100000, 1000000]:
+            reset(strategy)
+            metric_cache = _MetricCache(method=strategy)
+            timeit.timeit(command_store_foo, number=n)
+            print("%s\t%d\t%f\t%d" % (strategy, n, timeit.timeit(command_drain, number=1), metric_cache.size))
+
+    print("Benchmarking unique metric MetricCache drain...")
+    print("strategy cache_size time end_size")
+    for strategy in ["sorted", "max", "naive"]:
+        for n in [1000, 10000, 100000, 1000000]:
+            if strategy == "max" and n > 10000: # remove me when max is fast
+              print("%s\t%d\t%s\t%d" % (strategy, n, 'skipped', 0))
+              continue
+            reset(strategy)
+            metric_cache = _MetricCache(method=strategy)
+            timeit.timeit(command_store_foo_n, number=n)
+            print("%s\t%d\t%f\t%d" % (strategy, n, timeit.timeit(command_drain, number=1), metric_cache.size))

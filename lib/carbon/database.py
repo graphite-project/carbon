848e32b13606cd5dda58f96ffcd00793587cf7d0
Iain Buclaw <iain.buclaw@sociomantic.com>
2016-09-22 13:29:26 +0200

    Fix some British Englishness

--- lib/carbon/database.py
+++ lib/carbon/database.py
@@ -150,7 +150,7 @@ else:
 
     def __init__(self, settings):
       self.data_dir = settings.LOCAL_DATA_DIR
-      ceres.setDefaultSliceCachingBehavior(settings.CERES_SLICE_CACHING_BEHAVIOUR)
+      ceres.setDefaultSliceCachingBehavior(settings.CERES_SLICE_CACHING_BEHAVIOR)
       ceres.MAX_SLICE_GAP = int(settings.CERES_MAX_SLICE_GAP)
 
       if settings.CERES_LOCK_WRITES:

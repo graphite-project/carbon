a5946ca9cfd85e18dbdc689bebaeec799bf08077
Denys Zhdanov <denis.zhdanov@gmail.com>
2017-07-08 23:42:33 +0200

    Backporting fnv1a_hash ring from graphite-web
    It's not a full support of fnv1a hashing in carbon, it's only hashing part. I want to use that in carbonate for rebalance of fnv clusters.
    Completely not sure about python 3.x - my tests shows that fnv1a function accepts unicode and non unicode strings with same results but that need to be tested.

--- lib/carbon/hashing.py
+++ lib/carbon/hashing.py
@@ -5,27 +5,56 @@ except ImportError:
 import bisect
 import sys
 
+try:
+  import pyhash
+  hasher = pyhash.fnv1a_32()
+  def fnv32a(string, seed=0x811c9dc5):
+    return hasher(string, seed=seed)
+except ImportError:
+  def fnv32a(string, seed=0x811c9dc5):
+    """
+    FNV-1a Hash (http://isthe.com/chongo/tech/comp/fnv/) in Python.
+    Taken from https://gist.github.com/vaiorabbit/5670985
+    """
+    hval = seed
+    fnv_32_prime = 0x01000193
+    uint32_max = 2 ** 32
+    for s in string:
+      hval = hval ^ ord(s)
+      hval = (hval * fnv_32_prime) % uint32_max
+    return hval
 
 class ConsistentHashRing:
-  def __init__(self, nodes, replica_count=100):
+  def __init__(self, nodes, replica_count=100, hash_type='carbon_ch'):
     self.ring = []
     self.nodes = set()
     self.replica_count = replica_count
+    self.hash_type = hash_type
     for node in nodes:
       self.add_node(node)
 
   def compute_ring_position(self, key):
-    if sys.version_info >= (3, 0):
-      big_hash = md5(key.encode('utf-8')).hexdigest()
+    if self.hash_type == 'fnv1a_ch':
+      if sys.version_info >= (3, 0):
+        big_hash = '{:x}'.format(int(fnv32a(key)))
+      else:
+        big_hash = '{:x}'.format(int(fnv32a(str(key))))
+      small_hash = int(big_hash[:4], 16) ^ int(big_hash[4:], 16)
     else:
-      big_hash = md5(key).hexdigest()
-    small_hash = int(big_hash[:4], 16)
+      if sys.version_info >= (3, 0):
+        big_hash = md5(key.encode('utf-8')).hexdigest()
+      else:
+        big_hash = md5(key).hexdigest()
+      small_hash = int(big_hash[:4], 16)
     return small_hash
 
   def add_node(self, node):
     self.nodes.add(node)
     for i in range(self.replica_count):
-      replica_key = "%s:%d" % (node, i)
+      if self.hash_type == 'fnv1a_ch':
+        replica_key = "%d-%s" % (i, node[1])
+      else:
+        replica_key = "%s:%d" % (node, i)
       position = self.compute_ring_position(replica_key)
       while position in [r[0] for r in self.ring]:
         position = position + 1

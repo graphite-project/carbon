a8339ccb2bdcbf1d43d3574ba0b9c653c4b373e3
Denys Zhdanov <denis.zhdanov@gmail.com>
2017-07-09 00:35:06 +0200

    Linting tests

--- lib/carbon/tests/test_hashing.py
+++ lib/carbon/tests/test_hashing.py
@@ -169,14 +169,18 @@ class FNVHashIntegrityTest(unittest.TestCase):
 class ConsistentHashRingTestFNV1A(unittest.TestCase):
 
     def test_chr_compute_ring_position_fnv1a(self):
-        hosts = [("127.0.0.1", "ba603c36342304ed77953f84ac4d357b"),("127.0.0.2", "5dd63865534f84899c6e5594dba6749a"),
-        ("127.0.0.3", "866a18b81f2dc4649517a1df13e26f28")]
+        hosts = [("127.0.0.1", "ba603c36342304ed77953f84ac4d357b"),
+                 ("127.0.0.2", "5dd63865534f84899c6e5594dba6749a"),
+                 ("127.0.0.3", "866a18b81f2dc4649517a1df13e26f28")]
         hashring = ConsistentHashRing(hosts, hash_type='fnv1a_ch')
-        self.assertEqual(hashring.compute_ring_position('hosts.worker1.cpu'), 59573)
-        self.assertEqual(hashring.compute_ring_position('hosts.worker2.cpu'), 35749)
+        self.assertEqual(hashring.compute_ring_position('hosts.worker1.cpu'),
+                         59573)
+        self.assertEqual(hashring.compute_ring_position('hosts.worker2.cpu'),
+                         35749)
 
     def test_chr_get_node_fnv1a(self):
-        hosts = [("127.0.0.1", "ba603c36342304ed77953f84ac4d357b"), ("127.0.0.2", "5dd63865534f84899c6e5594dba6749a"),
+        hosts = [("127.0.0.1", "ba603c36342304ed77953f84ac4d357b"),
+                 ("127.0.0.2", "5dd63865534f84899c6e5594dba6749a"),
                  ("127.0.0.3", "866a18b81f2dc4649517a1df13e26f28")]
         hashring = ConsistentHashRing(hosts, hash_type='fnv1a_ch')
         self.assertEqual(hashring.get_node('hosts.worker1.cpu'),

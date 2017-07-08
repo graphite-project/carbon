1e3763d56c3104b6885a4dce057e9048a66237a5
Denys Zhdanov <denis.zhdanov@gmail.com>
2017-07-08 23:55:42 +0200

    Backporting fnv1a_hash ring from graphite-web to carbon 1.0.x
    It's not a full support of fnv1a hashing in carbon, it's only hashing part. I want to use that in carbonate for rebalance of fnv clusters.

--- lib/carbon/tests/test_hashing.py
+++ lib/carbon/tests/test_hashing.py
@@ -82,3 +82,85 @@ class HashIntegrityTest(unittest.TestCase):
         self.assertEqual(
                 len([n[0] for n in ring.ring]),
             len(set([n[0] for n in ring.ring])))
+
+
+class FNVHashIntegrityTest(unittest.TestCase):
+
+    def test_2_node_positional_itegrity(self):
+        """Make a cluster, verify we don't have positional collisions"""
+        ring = ConsistentHashRing([], hash_type='fnv1a_ch')
+        for n in range(2):
+            ring.add_node(("192.168.10.%s" % str(10+n),"%s" % str(10+n)))
+        self.assertEqual(
+                len([n[0] for n in ring.ring]),
+            len(set([n[0] for n in ring.ring])))
+
+
+    def test_3_node_positional_itegrity(self):
+        """Make a cluster, verify we don't have positional collisions"""
+        ring = ConsistentHashRing([], hash_type='fnv1a_ch')
+        for n in range(3):
+            ring.add_node(("192.168.10.%s" % str(10+n),"%s" % str(10+n)))
+        self.assertEqual(
+                len([n[0] for n in ring.ring]),
+            len(set([n[0] for n in ring.ring])))
+
+
+    def test_4_node_positional_itegrity(self):
+        """Make a cluster, verify we don't have positional collisions"""
+        ring = ConsistentHashRing([], hash_type='fnv1a_ch')
+        for n in range(4):
+            ring.add_node(("192.168.10.%s" % str(10+n),"%s" % str(10+n)))
+        self.assertEqual(
+                len([n[0] for n in ring.ring]),
+            len(set([n[0] for n in ring.ring])))
+
+
+    def test_5_node_positional_itegrity(self):
+        """Make a cluster, verify we don't have positional collisions"""
+        ring = ConsistentHashRing([], hash_type='fnv1a_ch')
+        for n in range(5):
+            ring.add_node(("192.168.10.%s" % str(10+n),"%s" % str(10+n)))
+        self.assertEqual(
+                len([n[0] for n in ring.ring]),
+            len(set([n[0] for n in ring.ring])))
+
+
+    def test_6_node_positional_itegrity(self):
+        """Make a cluster, verify we don't have positional collisions"""
+        ring = ConsistentHashRing([], hash_type='fnv1a_ch')
+        for n in range(6):
+            ring.add_node(("192.168.10.%s" % str(10+n),"%s" % str(10+n)))
+        self.assertEqual(
+                len([n[0] for n in ring.ring]),
+            len(set([n[0] for n in ring.ring])))
+
+
+    def test_7_node_positional_itegrity(self):
+        """Make a cluster, verify we don't have positional collisions"""
+        ring = ConsistentHashRing([], hash_type='fnv1a_ch')
+        for n in range(7):
+            ring.add_node(("192.168.10.%s" % str(10+n),"%s" % str(10+n)))
+        self.assertEqual(
+                len([n[0] for n in ring.ring]),
+            len(set([n[0] for n in ring.ring])))
+
+
+    def test_8_node_positional_itegrity(self):
+        """Make a cluster, verify we don't have positional collisions"""
+        ring = ConsistentHashRing([], hash_type='fnv1a_ch')
+        for n in range(8):
+            ring.add_node(("192.168.10.%s" % str(10+n),"%s" % str(10+n)))
+        self.assertEqual(
+                len([n[0] for n in ring.ring]),
+            len(set([n[0] for n in ring.ring])))
+
+
+    def test_9_node_positional_itegrity(self):
+        """Make a cluster, verify we don't have positional collisions"""
+        ring = ConsistentHashRing([], hash_type='fnv1a_ch')
+        for n in range(9):
+            ring.add_node(("192.168.10.%s" % str(10+n),"%s" % str(10+n)))
+        self.assertEqual(
+                len([n[0] for n in ring.ring]),
+            len(set([n[0] for n in ring.ring])))

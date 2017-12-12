graphite-project/carbon

commit 88a646441cabf7def6721546c62ef19e5a1c7e1e

Port Processor/Pipeline refactors from megacarbon
The 'megacarbon' branch contains a large refactor that implements the
transformation (aggregation, rewriting) and storage (cache) or relay as
a pipeline of processors. This change separates out that refactor from
the other changes in the branch without any compatibility changes that it
originally was paired with.

lib/carbon/writer.py
@@ -12,11 +12,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License."""
 
-
 import os
 import time
 from os.path import exists, dirname
-import errno
 
 import whisper
 from carbon import state
@@ -25,6 +23,7 @@ from carbon.storage import getFilesystemPath, loadStorageSchemas,\
     loadAggregationSchemas
 from carbon.conf import settings
 from carbon import log, events, instrumentation
+from carbon.util import TokenBucket
 
 from twisted.internet import reactor
 from twisted.internet.task import LoopingCall
@@ -36,63 +35,54 @@ except ImportError:
     log.debug("Couldn't import signal module")
 
 
-lastCreateInterval = 0
-createCount = 0
-schemas = loadStorageSchemas()
-agg_schemas = loadAggregationSchemas()
+SCHEMAS = loadStorageSchemas()
+AGGREGATION_SCHEMAS = loadAggregationSchemas()
 CACHE_SIZE_LOW_WATERMARK = settings.MAX_CACHE_SIZE * 0.95
+
+
+# Inititalize token buckets so that we can enforce rate limits on creates and
+# updates if the config wants them.
+CREATE_BUCKET = None
+UPDATE_BUCKET = None
+if settings.MAX_CREATES_PER_MINUTE != float('inf'):
+  capacity = settings.MAX_CREATES_PER_MINUTE
+  fill_rate = float(settings.MAX_CREATES_PER_MINUTE) / 60
+  CREATE_BUCKET = TokenBucket(capacity, fill_rate)
+
+if settings.MAX_UPDATES_PER_SECOND != float('inf'):
+  capacity = settings.MAX_UPDATES_PER_SECOND
+  fill_rate = settings.MAX_UPDATES_PER_SECOND
+  UPDATE_BUCKET = TokenBucket(capacity, fill_rate)
 
 
 def optimalWriteOrder():
   """Generates metrics with the most cached values first and applies a soft
   rate limit on new metrics"""
-  global lastCreateInterval
-  global createCount
-  metrics = MetricCache.counts()
-
-  t = time.time()
-  metrics.sort(key=lambda item: item[1], reverse=True)  # by queue size, descending
-  log.debug("Sorted %d cache queues in %.6f seconds" % (len(metrics),
-                                                        time.time() - t))
-
-  for metric, queueSize in metrics:
+  while MetricCache:
+    (metric, datapoints) = MetricCache.drain_metric()
     if state.cacheTooFull and MetricCache.size < CACHE_SIZE_LOW_WATERMARK:
       events.cacheSpaceAvailable()
 
     dbFilePath = getFilesystemPath(metric)
     dbFileExists = exists(dbFilePath)
 
-    if not dbFileExists:
-      createCount += 1
-      now = time.time()
-
-      if now - lastCreateInterval >= 60:
-        lastCreateInterval = now
-        createCount = 1
-
-      elif createCount >= settings.MAX_CREATES_PER_MINUTE:
-        # dropping queued up datapoints for new metrics prevents filling up the entire cache
-        # when a bunch of new metrics are received.
-        try:
-          MetricCache.pop(metric)
-        except KeyError:
-          pass
-        instrumentation.increment('droppedCreates')
-        continue
-
-    try:  # metrics can momentarily disappear from the MetricCache due to the implementation of MetricCache.store()
-      datapoints = MetricCache.pop(metric)
-    except KeyError:
-      log.msg("MetricCache contention, skipping %s update for now" % metric)
-      continue  # we simply move on to the next metric when this race condition occurs
+    if not dbFileExists and CREATE_BUCKET:
+      # If our tokenbucket has enough tokens available to create a new metric
+      # file then yield the metric data to complete that operation. Otherwise
+      # we'll just drop the metric on the ground and move on to the next
+      # metric.
+      # XXX This behavior should probably be configurable to no tdrop metrics
+      # when rate limitng unless our cache is too big or some other legit
+      # reason.
+      if CREATE_BUCKET.drain(1):
+        yield (metric, datapoints, dbFilePath, dbFileExists)
+      continue
 
     yield (metric, datapoints, dbFilePath, dbFileExists)
 
 
 def writeCachedDataPoints():
   "Write datapoints until the MetricCache is completely empty"
-  updates = 0
-  lastSecond = 0
 
   while MetricCache:
     dataWritten = False
@@ -104,13 +94,13 @@ def writeCachedDataPoints():
         archiveConfig = None
         xFilesFactor, aggregationMethod = None, None
 
-        for schema in schemas:
+        for schema in SCHEMAS:
           if schema.matches(metric):
             log.creates('new metric %s matched schema %s' % (metric, schema.name))
             archiveConfig = [archive.getTuple() for archive in schema.archives]
             break
 
-        for schema in agg_schemas:
+        for schema in AGGREGATION_SCHEMAS:
           if schema.matches(metric):
             log.creates('new metric %s matched aggregation schema %s' % (metric, schema.name))
             xFilesFactor, aggregationMethod = schema.archives
@@ -121,25 +111,32 @@ def writeCachedDataPoints():
 
         dbDir = dirname(dbFilePath)
         try:
-          os.makedirs(dbDir)
-        except OSError as e:
-          if e.errno != errno.EEXIST:
+            if not exists(dbDir):
+                os.makedirs(dbDir)
+        except OSError, e:
             log.err("%s" % e)
         log.creates("creating database file %s (archive=%s xff=%s agg=%s)" %
                     (dbFilePath, archiveConfig, xFilesFactor, aggregationMethod))
         try:
-          whisper.create(dbFilePath, archiveConfig, xFilesFactor, aggregationMethod, settings.WHISPER_SPARSE_CREATE, settings.WHISPER_FALLOCATE_CREATE)
-          instrumentation.increment('creates')
-        except Exception, e:
-          log.err("Error creating %s: %s" % (dbFilePath,e))
-          continue
-
+            whisper.create(
+                dbFilePath,
+                archiveConfig,
+                xFilesFactor,
+                aggregationMethod,
+                settings.WHISPER_SPARSE_CREATE,
+                settings.WHISPER_FALLOCATE_CREATE)
+            instrumentation.increment('creates')
+        except:
+            log.err("Error creating %s" % (dbFilePath))
+            continue
+      # If we've got a rate limit configured lets makes sure we enforce it
+      if UPDATE_BUCKET:
+        UPDATE_BUCKET.drain(1, blocking=True)
       try:
         t1 = time.time()
         whisper.update_many(dbFilePath, datapoints)
-        t2 = time.time()
-        updateTime = t2 - t1
-      except:
+        updateTime = time.time() - t1
+      except Exception:
         log.msg("Error writing to %s" % (dbFilePath))
         log.err()
         instrumentation.increment('errors')
@@ -147,20 +144,8 @@ def writeCachedDataPoints():
         pointCount = len(datapoints)
         instrumentation.increment('committedPoints', pointCount)
         instrumentation.append('updateTimes', updateTime)
-
         if settings.LOG_UPDATES:
           log.updates("wrote %d datapoints for %s in %.5f seconds" % (pointCount, metric, updateTime))
-
-        # Rate limit update operations
-        thisSecond = int(t2)
-
-        if thisSecond != lastSecond:
-          lastSecond = thisSecond
-          updates = 0
-        else:
-          updates += 1
-          if updates >= settings.MAX_UPDATES_PER_SECOND:
-            time.sleep(int(t2 + 1) - t2)
 
     # Avoid churning CPU when only new metrics are in the cache
     if not dataWritten:
@@ -171,33 +156,36 @@ def writeForever():
   while reactor.running:
     try:
       writeCachedDataPoints()
-    except:
+    except Exception:
       log.err()
-
     time.sleep(1)  # The writer thread only sleeps when the cache is empty or an error occurs
 
 
 def reloadStorageSchemas():
-  global schemas
+  global SCHEMAS
   try:
-    schemas = loadStorageSchemas()
-  except:
-    log.msg("Failed to reload storage schemas")
+    SCHEMAS = loadStorageSchemas()
+  except Exception:
+    log.msg("Failed to reload storage SCHEMAS")
     log.err()
 
 
 def reloadAggregationSchemas():
-  global agg_schemas
+  global AGGREGATION_SCHEMAS
   try:
-    agg_schemas = loadAggregationSchemas()
-  except:
-    log.msg("Failed to reload aggregation schemas")
+    AGGREGATION_SCHEMAS = loadAggregationSchemas()
+  except Exception:
+    log.msg("Failed to reload aggregation SCHEMAS")
     log.err()
 
 
 def shutdownModifyUpdateSpeed():
     try:
-        settings.MAX_UPDATES_PER_SECOND = settings.MAX_UPDATES_PER_SECOND_ON_SHUTDOWN
+        shut = settings.MAX_UPDATES_PER_SECOND_ON_SHUTDOWN
+        if UPDATE_BUCKET:
+          UPDATE_BUCKET.setCapacityAndFillRate(shut,shut)
+        if CREATE_BUCKET:
+          CREATE_BUCKET.setCapacityAndFillRate(shut,shut)
         log.msg("Carbon shutting down.  Changed the update rate to: " + str(settings.MAX_UPDATES_PER_SECOND_ON_SHUTDOWN))
     except KeyError:
         log.msg("Carbon shutting down.  Update rate not changed")

conf/carbon.conf.example
@@ -95,6 +95,31 @@ USE_FLOW_CONTROL = True
 LOG_UPDATES = False
 LOG_CACHE_HITS = False
 
+# The thread that writes metrics to disk can use on of the following strategies
+# determining the order in which metrics are removed from cache and flushed to
+# disk. The default option preserves the same behavior as has been historically
+# available in version 0.9.10.
+#
+# sorted - All metrics in the cache will be counted and an ordered list of
+# them will be sorted according to the number of datapoints in the cache at the
+# moment of the list's creation. Metrics will then be flushed from the cache to
+# disk in that order.
+#
+# max - The writer thread will always pop and flush the metric from cache
+# that has the most datapoints. This will give a strong flush preference to
+# frequently updated metrics and will also reduce random file-io. Infrequently
+# updated metrics may only ever be persisted to disk at daemon shutdown if
+# there are a large number of metrics which receive very frequent updates OR if
+# disk i/o is very slow.
+#
+# naive - Metrics will be flushed from the cache to disk in an unordered
+# fashion. This strategy may be desirable in situations where the storage for
+# whisper files is solid state, CPU resources are very limited or deference to
+# the OS's i/o scheduler is expected to compensate for the random write
+# pattern.
+#
+CACHE_WRITE_STRATEGY = sorted
+
 # On some systems it is desirable for whisper to write synchronously.
 # Set this option to True if you'd like to try this. Basically it will
 # shift the onus of buffering writes from the kernel into carbon's cache.

lib/carbon/aggregator/buffers.py
@@ -51,7 +51,7 @@ class MetricBuffer:
     self.aggregation_frequency = int(frequency)
     self.aggregation_func = func
     self.compute_task = LoopingCall(self.compute_value)
-    self.compute_task.start(settings['WRITE_BACK_FREQUENCY'] or frequency, now=False)
+    self.compute_task.start(settings['WRITE_BACK_FREQUENCY'] or int(frequency), now=False)
     self.configured = True
 
   def compute_value(self):

lib/carbon/aggregator/processor.py
@@ -1,0 +1,38 @@
+from carbon.aggregator.rules import RuleManager
+from carbon.aggregator.buffers import BufferManager
+from carbon.instrumentation import increment
+from carbon.pipeline import Processor
+from carbon.rewrite import PRE, POST, RewriteRuleManager
+
+
+class AggregationProcessor(Processor):
+  plugin_name = 'aggregate'
+
+  def process(self, metric, datapoint):
+    increment('datapointsReceived')
+
+    for rule in RewriteRuleManager.rules(PRE):
+      metric = rule.apply(metric)
+
+    aggregate_metrics = set()
+
+    for rule in RuleManager.rules:
+      aggregate_metric = rule.get_aggregate_metric(metric)
+
+      if aggregate_metric is None:
+        continue
+      else:
+        aggregate_metrics.add(aggregate_metric)
+
+      values_buffer = BufferManager.get_buffer(aggregate_metric)
+
+      if not values_buffer.configured:
+        values_buffer.configure_aggregation(rule.frequency, rule.aggregation_func)
+
+      values_buffer.input(datapoint)
+
+    for rule in RewriteRuleManager.rules(POST):
+      metric = rule.apply(metric)
+
+    if metric not in aggregate_metrics:
+      yield (metric, datapoint)

lib/carbon/aggregator/receiver.py
@@ -1,39 +1,0 @@
-from carbon.instrumentation import increment
-from carbon.aggregator.rules import RuleManager
-from carbon.aggregator.buffers import BufferManager
-from carbon.rewrite import RewriteRuleManager
-from carbon.conf import settings
-from carbon import events, log
-
-
-def process(metric, datapoint):
-  increment('datapointsReceived')
-
-  for rule in RewriteRuleManager.preRules:
-    metric = rule.apply(metric)
-
-  aggregate_metrics = []
-
-  for rule in RuleManager.rules:
-    aggregate_metric = rule.get_aggregate_metric(metric)
-
-    if aggregate_metric is None:
-      continue
-    else:
-      aggregate_metrics.append(aggregate_metric)
-
-    buffer = BufferManager.get_buffer(aggregate_metric)
-
-    if not buffer.configured:
-      buffer.configure_aggregation(rule.frequency, rule.aggregation_func)
-
-    buffer.input(datapoint)
-
-  for rule in RewriteRuleManager.postRules:
-    metric = rule.apply(metric)
-
-  if metric not in aggregate_metrics:
-    events.metricGenerated(metric, datapoint)
-
-  if settings.LOG_AGGREGATOR_MISSES and len(aggregate_metrics) == 0:
-    log.msg("Couldn't match metric %s with any aggregation rule. Passing on un-aggregated." % metric)

lib/carbon/cache.py
@@ -12,53 +12,151 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License."""
 
-from threading import Lock
+import time
+from operator import itemgetter
+from random import choice
+
 from carbon.conf import settings
+from carbon import events, log
+from carbon.pipeline import Processor
 
 
-class MetricCache(dict):
-  def __init__(self):
+def by_timestamp((timestamp, value)):  # useful sort key function
+  return timestamp
+
+
+class CacheFeedingProcessor(Processor):
+  plugin_name = 'write'
+
+  def process(self, metric, datapoint):
+    MetricCache.store(metric, datapoint)
+    return Processor.NO_OUTPUT
+
+
+class DrainStrategy(object):
+  """Implements the strategy for writing metrics.
+  The strategy chooses what order (if any) metrics
+  will be popped from the backing cache"""
+  def __init__(self, cache):
+    self.cache = cache
+
+  def choose_item(self):
+    raise NotImplemented
+
+
+class MaxStrategy(DrainStrategy):
+  """Always pop the metric with the greatest number of points stored.
+  This method leads to less variance in pointsPerUpdate but may mean
+  that infrequently or irregularly updated metrics may not be written
+  until shutdown """
+  def choose_item(self):
+    metric_name, size = max(self.cache.items(), key=lambda x: len(itemgetter(1)(x)))
+    return metric_name
+
+
+class RandomStrategy(DrainStrategy):
+  """Pop points randomly"""
+  def choose_item(self):
+    return choice(self.cache.keys())
+
+
+class SortedStrategy(DrainStrategy):
+  """ The default strategy which prefers metrics with a greater number
+  of cached points but guarantees every point gets written exactly once during
+  a loop of the cache """
+  def __init__(self, cache):
+    super(SortedStrategy, self).__init__(cache)
+
+    def _generate_queue():
+      while True:
+        t = time.time()
+        metric_counts = sorted(self.cache.counts, key=lambda x: x[1])
+        log.debug("Sorted %d cache queues in %.6f seconds" % (len(metric_counts), time.time() - t))
+        while metric_counts:
+          yield itemgetter(0)(metric_counts.pop())
+
+    self.queue = _generate_queue()
+
+  def choose_item(self):
+    return self.queue.next()
+
+
+class _MetricCache(dict):
+  """A Singleton dictionary of metric names and lists of their datapoints"""
+  def __init__(self, strategy=None):
     self.size = 0
-    self.lock = Lock()
+    self.strategy = None
+    if strategy:
+      self.strategy = strategy(self)
 
   def __setitem__(self, key, value):
     raise TypeError("Use store() method instead!")
 
-  def store(self, metric, datapoint):
-    try:
-      self.lock.acquire()
-      self.setdefault(metric, []).append(datapoint)
-      self.size += 1
-    finally:
-      self.lock.release()
+  @property
+  def counts(self):
+    return [(metric, len(datapoints)) for (metric, datapoints) in self.items()]
 
-    if self.isFull():
-      log.msg("MetricCache is full: self.size=%d" % self.size)
-      state.events.cacheFull()
+  @property
+  def is_full(self):
+    if settings.MAX_CACHE_SIZE == float('inf'):
+      return False
+    else:
+      return self.size >= settings.MAX_CACHE_SIZE
 
-  def isFull(self):
-    return self.size >= settings.MAX_CACHE_SIZE
+  def _check_available_space(self):
+    if state.cacheTooFull and self.size < settings.CACHE_SIZE_LOW_WATERMARK:
+      log.msg("cache size below watermark")
+      events.cacheSpaceAvailable()
+
+  def drain_metric(self):
+    """Returns a metric and it's datapoints in order determined by the
+    `DrainStrategy`_"""
+    if not self:
+      return (None, [])
+    if self.strategy:
+      metric = self.strategy.choose_item()
+    else:
+      # Avoid .keys() as it dumps the whole list
+      metric = self.iterkeys().next()
+    return (metric, self.pop(metric))
+
+  def get_datapoints(self, metric):
+    """Return a list of currently cached datapoints sorted by timestamp"""
+    return sorted(self.get(metric, {}).items(), key=by_timestamp)
 
   def pop(self, metric):
-    try:
-      self.lock.acquire()
-      datapoints = dict.pop(self, metric)
-      self.size -= len(datapoints)
-      return datapoints
-    finally:
-      self.lock.release()
+    datapoint_index = dict.pop(self, metric)
+    self.size -= len(datapoint_index)
+    self._check_available_space()
 
-  def counts(self):
-    try:
-      self.lock.acquire()
-      return [ (metric, len(datapoints)) for (metric, datapoints) in self.items() ]
-    finally:
-      self.lock.release()
+    return sorted(datapoint_index.items(), key=by_timestamp)
+
+  def store(self, metric, datapoint):
+    self.setdefault(metric, {})
+    timestamp, value = datapoint
+    if timestamp not in self[metric]:
+      # Not a duplicate, hence process if cache is not full
+      if self.is_full:
+        log.msg("MetricCache is full: self.size=%d" % self.size)
+        events.cacheFull()
+      else:
+        self.size += 1
+        self[metric][timestamp] = value
+    else:
+      # Updating a duplicate does not increase the cache size
+      self[metric][timestamp] = value
 
 
-# Ghetto singleton
-MetricCache = MetricCache()
+# Initialize a singleton cache instance
+write_strategy = None
+if settings.CACHE_WRITE_STRATEGY == 'max':
+  write_strategy = MaxStrategy
+if settings.CACHE_WRITE_STRATEGY == 'sorted':
+  write_strategy = SortedStrategy
+if settings.CACHE_WRITE_STRATEGY == 'random':
+  write_strategy = RandomStrategy
 
+MetricCache = _MetricCache(write_strategy)
 
 # Avoid import circularities
-from carbon import log, state
+from carbon import state

lib/carbon/client.py
@@ -1,3 +1,6 @@
+from collections import deque
+from time import time
+
 from twisted.application.service import Service
 from twisted.internet import reactor
 from twisted.internet.defer import Deferred, DeferredList
@@ -5,9 +8,7 @@ from twisted.internet.protocol import ReconnectingClientFactory
 from twisted.protocols.basic import Int32StringReceiver
 from carbon.conf import settings
 from carbon.util import pickle
-from carbon import log, state, instrumentation
-from collections import deque
-from time import time
+from carbon import instrumentation, log, pipeline, state
 
 try:
     import signal
@@ -384,3 +385,11 @@ class CarbonClientManager(Service):
 
   def __str__(self):
     return "<%s[%x]>" % (self.__class__.__name__, id(self))
+
+
+class RelayProcessor(pipeline.Processor):
+  plugin_name = 'relay'
+
+  def process(self, metric, datapoint):
+    state.client_manager.sendDatapoint(metric, datapoint)
+    return pipeline.Processor.NO_OUTPUT

lib/carbon/conf.py
@@ -61,7 +61,7 @@ defaults = dict(
   MANHOLE_PORT=7222,
   MANHOLE_USER="",
   MANHOLE_PUBLIC_KEY="",
-  RELAY_METHOD='rules',
+  RELAY_METHOD='relay-rules',
   REPLICATION_FACTOR=1,
   DESTINATIONS=[],
   USE_FLOW_CONTROL=True,
@@ -69,6 +69,7 @@ defaults = dict(
   USE_WHITELIST=False,
   CARBON_METRIC_PREFIX='carbon',
   CARBON_METRIC_INTERVAL=60,
+  CACHE_WRITE_STRATEGY='sorted',
   WRITE_BACK_FREQUENCY=None,
   MIN_RESET_STAT_FLOW=1000,
   MIN_RESET_RATIO=0.9,

commit d92ead96b872d7fba4599badeba0eecda17449e5

restore droppedCreates metric
the refactor in 88a6464 accidentally stopped incremeting dropedCreates

lib/carbon/writer.py
@@ -110,6 +110,7 @@ def writeCachedDataPoints():
         # XXX This behavior should probably be configurable to no tdrop metrics
         # when rate limitng unless our cache is too big or some other legit
         # reason.
+        instrumentation.increment('droppedCreates')
         continue
 
       archiveConfig = None
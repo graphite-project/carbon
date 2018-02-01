dd9d23de6320076882bef1361091fb6d412839b9
Dan Cech <dcech@grafana.com>
2018-02-01 17:05:50 -0500

    codacy fix

--- lib/carbon/tests/test_database.py
+++ lib/carbon/tests/test_database.py
@@ -39,8 +39,8 @@ class WhisperDatabaseTest(TestCase):
         result = database.getFilesystemPath('stats.example.counts;tag1=value1')
         self.assertEqual(
             result,
-            '/tmp/_tagged/872/252/' +
-            '872252dcead671982862f82a3b440f02aa8f525dd6d0f2921de0dc2b3e874ad0.wsp')  # nosec
+            '/tmp/_tagged/872/252/' +  # nosec
+            '872252dcead671982862f82a3b440f02aa8f525dd6d0f2921de0dc2b3e874ad0.wsp')
 
 
 class CeresDatabaseTest(TestCase):
@@ -76,5 +76,5 @@ class CeresDatabaseTest(TestCase):
         result = database.getFilesystemPath('stats.example.counts;tag1=value1')
         self.assertEqual(
             result,
-            '/tmp/_tagged/872/252/' +
-            '872252dcead671982862f82a3b440f02aa8f525dd6d0f2921de0dc2b3e874ad0')  # nosec
+            '/tmp/_tagged/872/252/' +  # nosec
+            '872252dcead671982862f82a3b440f02aa8f525dd6d0f2921de0dc2b3e874ad0')

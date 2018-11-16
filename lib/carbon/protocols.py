f3522ca69e29d769469e1813e0f4cedd5e7c0d65
Semyon Slepov <semyon.slepov@smarkets.com>
2018-11-16 12:13:24 +0000

    Make max pickle string length configurable

--- lib/carbon/protocols.py
+++ lib/carbon/protocols.py
@@ -234,7 +234,10 @@ class MetricDatagramReceiver(MetricReceiver, DatagramProtocol):
 
 class MetricPickleReceiver(MetricReceiver, Int32StringReceiver):
   plugin_name = "pickle"
-  MAX_LENGTH = 2 ** 20
+
+  def __init__(self):
+    super(MetricPickleReceiver, self).__init__()
+    self.MAX_LENGTH = settings.MAX_PICKLE_STRING_LENGTH
 
   def connectionMade(self):
     MetricReceiver.connectionMade(self)

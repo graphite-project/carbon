abf477cd54465736c0ffb3414b5ce256636fe666
shehani009 <shehanikandy@yahoo.com>
2020-10-06 19:40:37 +0530

    array

---
+++ program/array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>  
+int main(){      
+int i=0;    
+int marks[5];//declaration of array       
+marks[0]=80;//initialization of array    
+marks[1]=60;    
+marks[2]=70;    
+marks[3]=85;    
+marks[4]=75;    
+//traversal of array    
+for(i=0;i<5;i++){      
+printf("%d \n",marks[i]);    
+}//end of for loop     
+return 0;  
+}    
